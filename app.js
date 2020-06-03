70589ac14c4c55061d3a41aff0ee5389cebeef6b
Girujan Naguleswaran <ngirujan@gmail.com>
2020-06-03 02:30:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,32 +10,26 @@
   firebase.initializeApp(firebaseConfig);
 
   var provider = new firebase.auth.GoogleAuthProvider();
+  
+  const auth = firebase.auth();
 
-  const loginBtn = document.getElementById('btnLogin');
+  const promise = auth.signInWithPopup(provider);
 
-  loginBtn.addEventListener('click', e => {
-
-    const auth = firebase.auth();
-
-    const promise = auth.signInWithPopup(provider);
-
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+  // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
     // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-
   });
 
 } ());
